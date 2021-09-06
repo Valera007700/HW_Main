2913adb4d286ae85fe4a8068532d2961a71a8131
valery <valeradoroshkov987@gmail.com>
2021-09-06 20:51:51 +0300

    Home_Work_3

--- MATH.py
+++ MATH.py
@@ -1,5 +1,11 @@
-a = int(input())
-if a < -5:
-    print("Low")
-else:
-    print('High')
+n = input("Введите трехзначное число: ")
+n = int(n)
+
+d1 = n % 10
+n = n // 10
+d2 = n % 10
+n = n // 10
+d3 = n % 10
+n = n // 10
+
+print("Сумма цифр числа:", n + d2 + d3)
