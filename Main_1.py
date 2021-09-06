d7cc449fa7f950c132f4c1dfe4790b28d9be8f29
valery <valeradoroshkov987@gmail.com>
2021-09-06 18:12:58 +0300

    Home_Work_3

--- Main_1.py
+++ Main_1.py
@@ -1,3 +1,27 @@
-while True:
-    name = input()
-    print('Your input word =', name)
+name_1 = "Alex"
+print(print(name_1), type(name_1))
+
+name_2 = 4654
+print( print(name_2), type(name_2))
+
+name_3 = 22.8
+print(type(name_3), print(name_3))
+
+name_4 = bytes([54, 89])
+print(chr(54))
+print(type(name_4), print(name_4))
+
+name_5 = list('Список')
+print(name_5, type(name_5), print(name_5))
+
+name_6 = tuple('Тапки не меняются ')
+print(name_6, type(name_6), print(name_6))
+
+name_7 = set('Рандомные буквы без повторов')
+print(name_7, type(name_7), print(name_7))
+
+name_8 = frozenset('Рандомные буквы без повторов')
+print(name_8, type(name_8), print(name_8))
+
+name_9 = {"first": 1, "second": 2}
+print(type(name_9), name_9)
