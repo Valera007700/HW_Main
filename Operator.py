dc7bf01a49367608343d133c4e35da2dd4ca3908
valery <valeradoroshkov987@gmail.com>
2021-09-10 23:40:47 +0300

    Operator.py

--- Operator.py
+++ Operator.py
@@ -1,10 +1,35 @@
-# b_true = True
-# b_false = False
+# # b_true = True
+# # b_false = False
+#
+# age = 26
+# weight = 48
+# qa = True
+#
+# name = 'Julia'
+# result = name + ' ' + str(age) + ' ' + str(weight)
+# print(result)
+#
+#
+# # a = int(input())
+# # if a < -5:
+# #     print('Low')
+# # elif -5 <= a <= 5:
+# #     print('Mid')
+# # else:
+# #     print('High')
 
-age = 26
-weight = 48
-qa = True
+# a = int(input())
+# if a < 14:
+#     print('Cold');
+# elif 15 < a < 22:
+#     print("normal")
+# else:
+#     print("super")
 
-name = 'Julia'
-result = name + ' ' + str(age) + ' ' + str(weight)
-print(result)
+a = int(input())
+if a < 100:
+    print("super")
+elif 100 > a > 500:
+    print("normal")
+else:
+    print("may be")
